92f60a805cb5576aa4d0f88880de96d722a5222e
Varun Peram <52163292+varunperam@users.noreply.github.com>
2023-01-29 00:41:39 +0530

    Updated all the files

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ hamburger.onclick = function () {
     let navBar = document.querySelector(".nav-bar");
     navBar.classList.toggle("active");
 }
+
+function fnResume(type) {
+    let i, j, tabContent, tabLinks;
+
+    tabLinks = document.getElementsByClassName("tab-btn");
+
+    for (j = 0; j < tabLinks.length; j++) {
+        tabLinks[j].style.backgroundColor = "#3C526F";
+    }
+    tabContent = document.getElementsByClassName("resume-fi");
+
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+
+    document.getElementById(type).style.display = "block";
+    document.getElementById(type + "btn1").style.backgroundColor = "#274060";
+}
